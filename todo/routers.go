f36945205f37c85699fe62f4c986ff9878cf3eaa
Patryk Przekwas <pkprzekwas@gmail.com>
2019-10-14 22:02:42 +0200

    Todo routing fix

--- todo/routers.go
+++ todo/routers.go
@@ -4,9 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(r *gin.RouterGroup) {
 	r.GET("todo/:id", Get)
-	r.GET("/", List)
-	r.POST("/", Create)
-	r.PUT("/:id", Update)
-	r.DELETE("/:id", Delete)
+	r.GET("todo/", List)
+	r.POST("todo/", Create)
+	r.PUT("todo/:id", Update)
+	r.DELETE("todo/:id", Delete)
 }
 
