b5287eddbe1c110f8a3dfc94e3edf0a3a3caa9d0
Patryk Przekwas <pkprzekwas@gmail.com>
2019-10-15 06:59:59 +0200

    Frontend init

---
+++ front/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Layout msg="Welcome"/>
+  </div>
+</template>
+
+<script>
+import Layout from './components/Layout.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style>
+  * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    background-color: #171717;
+    color: white;
+    padding: 100px 50px;
+  }
+
+  #app {
+    font-family: 'Fira Code', sans-serif;
+    text-align: center;
+    margin-top: 60px;
+  }
+</style>
